8a0c53a8d24b990414f486b26347e21e81297263
Gil Tayar <gilt@wix.com>
2016-04-02 20:12:15 +0300

    Merged simplified object and fields snippets

--- 01-tutorial-code/02-es6/04-simplified-object.js
+++ 01-tutorial-code/02-es6/04-simplified-object.js
@@ -2,26 +2,29 @@ const fs = require('fs')
 const os = require('os')
 const path = require('path')
 
+const sourceFile = path.join(__dirname, 'data/hello-world.txt')
+const targetFile = path.join(os.tmpdir(), 'copied-file.txt')
+
 const operations = {
-  copyFile(sourceFile, targetFile, cb) {
-    fs.readFile(sourceFile, (err, contentBuffer) => {
+  copyFile(cb) {
+    fs.readFile(this.sourceFile, (err, contentBuffer) => {
       if (err)
         return cb(err)
         
-      fs.writeFile(targetFile, contentBuffer, err => {
+      fs.writeFile(this.targetFile, contentBuffer, err => {
         if (err)
           return cb(err)
         
         cb()
       })
     })
-  }    
+  },
+  sourceFile,
+  targetFile
 }
 
-const sourceFile = path.join(__dirname, 'data/hello-world.txt')
-const targetFile = path.join(os.tmpdir(), 'copied-file.txt')
 
-operations.copyFile(sourceFile, targetFile, err => {
+operations.copyFile(err => {
     fs.readFile(targetFile, {encoding: 'utf-8'}, (err, content) =>
       console.log(content)
     )
