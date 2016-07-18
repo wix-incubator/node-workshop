07633dab334e0810ef256420656338e60ac129f3
Eyal mrejen <eyalm@wix.com>
2016-07-18 13:58:55 +0300

    fixing missing radix issue

--- 01-tutorial-code/01-intro-to-nodejs/12-express-req-query.js
+++ 01-tutorial-code/01-intro-to-nodejs/12-express-req-query.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/add', function(req, res) {
-  res.send((parseInt(req.query.a) + parseInt(req.query.b)).toString())
+  res.send((parseInt(req.query.a, 10) + parseInt(req.query.b, 10)).toString())
 })
 
 const server = app.listen(process.env.PORT || 3000, function() {
